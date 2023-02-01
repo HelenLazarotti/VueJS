3d072f52e9b4d2864b282239e8f5fb39154b34c8
Helen Lazarotti <74218577+HelenLazarotti@users.noreply.github.com>
2023-02-01 11:21:26 -0300

    Módulo 4 pronto

---
+++ vue-cli/projeto-helen1/src/App.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/valid-template-root -->
+// eslint-disable-next-line vue/valid-template-root
+<template>
+    <div>
+        {{ titulo }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            titulo: 'Teste Usando VueJs com data-Helen'
+        }
+    }
+}
+</script>
