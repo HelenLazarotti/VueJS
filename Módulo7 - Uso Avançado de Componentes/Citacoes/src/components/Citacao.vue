1521a3b5d71edd32a9ef063864fc329162a3568b
Helen Lazarotti <74218577+HelenLazarotti@users.noreply.github.com>
2023-02-02 20:14:55 -0300

    Ultimas atualizações

---
+++ Módulo7 - Uso Avançado de Componentes/Citacoes/src/components/Citacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="citacao">
+        <!--Não importa a ordem-->
+        <slot name="fonte"></slot>
+        <slot>Citação:</slot>
+        <slot name="texto"></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
