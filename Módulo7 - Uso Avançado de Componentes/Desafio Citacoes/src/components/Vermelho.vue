9716ce78d6ecd2513648f915d75cfd4d732c9393
Helen Lazarotti <helen.lazarotti@gmail.com>
2023-02-03 10:39:29 -0300

    Módulo 7 pronto com desafio

---
+++ Módulo7 - Uso Avançado de Componentes/Desafio Citacoes/src/components/Vermelho.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="caixa vermelho" slot="vermleho">
+        <slot name="conteudo"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
